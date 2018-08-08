c2f7bce5f4bb707292b439318e1fa04dc5c399e6
MingJiang3 <457918455@qq.com>
2018-08-08 17:11:39 +0800

    优化按钮和代码

--- canvas/main.js
+++ canvas/main.js
@@ -1,70 +1,76 @@
 window.onload = function () {
     var yyy = document.getElementById('xxx');
-    pageSize()
-    window.onresize = function(){
-        pageSize()
-    }
-    function pageSize (){
-        var pageWidth = document.documentElement.clientWidth
-        var pageHeight = document.documentElement.clientHeight
-        yyy.width = pageWidth
-        yyy.height = pageHeight
-    }
-
     var context = yyy.getContext('2d');
-    var using = false
-    var lastPoint = {x:undefined,y:undefined}
+    autosetCanvasSize(yyy)
+    listenToMouse(yyy)
 
-    yyy.onmousedown = function (aaa) {
-        var x = aaa.clientX
-        var y = aaa.clientY
-        if(eraserEnabled){
-            using = true
-            context.clearRect(x-5,y-5,10,10)
-        }else{
-            using = true
-            lastPoint = {'x':x,'y':y}
+   //橡皮擦和画笔的切换。尽量不要用一个按钮做两件事
+    var eraserEnabled = false
+    eraser.onclick = function () {
+        eraserEnabled = true
+        actions.className = 'actions x'
+    }
+    brush.onclick = function () {
+        eraserEnabled = false
+        actions.className = 'actions'
+    }
+    /* *************** */
+    function autosetCanvasSize(canvas) {
+        setCanvasSize()
+        window.onresize = function () {
+            setCanvasSize()
+        }
+        function setCanvasSize() {
+            var pageWidth = document.documentElement.clientWidth
+            var pageHeight = document.documentElement.clientHeight
+            yyy.width = pageWidth
+            yyy.height = pageHeight
         }
     }
-    yyy.onmousemove = function (aaa) {
-        var x = aaa.clientX
-        var y = aaa.clientY
-        if(eraserEnabled){
-            if(using){
-                context.clearRect(x-5,y-5,10,10)
+    function listenToMouse(canvas) {
+        var using = false
+        var lastPoint = { x: undefined, y: undefined }
+        canvas.onmousedown = function (aaa) {
+            var x = aaa.clientX
+            var y = aaa.clientY
+            using = true
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                lastPoint = { 'x': x, 'y': y }
             }
-        }else{
-            if(using){
-                var newPoint = {'x':x,'y':y}
+        }
+        canvas.onmousemove = function (aaa) {
+            var x = aaa.clientX
+            var y = aaa.clientY
+            if(!using){return}
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)               
+            } else {
+                var newPoint = { 'x': x, 'y': y }
                 // drawCircle(x, y, 1)
-                drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-                lastPoint = newPoint
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint                
             }
         }
-    }
-    yyy.onmouseup = function (aaa) {
-        using = false
+        canvas.onmouseup = function (aaa) {
+            using = false
+        }
     }
     function drawCircle(x, y, radius) {
         context.beginPath()
         context.arc(x, y, radius, 0, Math.PI * 2);
         context.fill()
     }
-    function drawLine(x1,y1,x2,y2){
+    function drawLine(x1, y1, x2, y2) {         //用线把前后两个点链接起来
         context.beginPath();
-        context.moveTo(x1,y1)       //起点
-        context.lineTo(x2,y2)       //终点
+        context.moveTo(x1, y1)       //起点
+        context.lineTo(x2, y2)       //终点
         context.stroke();
         context.closePath()
         context.lineWidth = 2
     }
 
-    var eraserEnabled = false
-    eraser.onclick = function(){
-        console.log('aaaaa')
-        eraserEnabled = !eraserEnabled
-
-    }
 }
 
 
