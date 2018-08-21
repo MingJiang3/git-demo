1a6b51a747f93ca53119491d1965f09af2aa097a
MingJiang3 <457918455@qq.com>
2018-08-21 18:27:40 +0800

    jQuery轮播

--- slideShow/main.js
+++ slideShow/main.js
@@ -1,12 +1,26 @@
-$('#bt1').on('click',function(){
-    $(images).css({transform: 'translateX(0px)'})
-})
-$('#bt2').on('click',function(){
-    $(images).css({
-        transform: 'translateX(-500px)'
+// $('#bt1').on('click',function(){
+//     $('.images').css({transform: 'translateX(0px)'})
+// })
+// $('#bt2').on('click',function(){
+//     $('.images').css({
+//         transform: 'translateX(-500px)'
+//     })
+// })
+// $('#bt3').on('click',function(){
+//     $('.images').css({transform: 'translateX(-1000px)'})
+// })
+
+var buttonAll = $('#buttons > button')
+for (let i = 0; i < buttonAll.length; i++) {
+    $(buttonAll[i]).on('click',function(x){
+        console.log(x)
+        var index = $(x.currentTarget).index()
+        var n = index * -500
+        $('.images').css({transform: 'translate('+n+'px)'})
     })
-})
-$('#bt3').on('click',function(){
-    console.log("111")
-    $(images).css({transform: 'translateX(-1000px)'})
-})
+    
+}
+
+
+
+
