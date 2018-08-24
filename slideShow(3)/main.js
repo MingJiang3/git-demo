69218826a9f98182cdc6d24dc102031cf71c7ec0
MingJiang3 <457918455@qq.com>
2018-08-24 22:53:18 +0800

    1

---
+++ slideShow(3)/main.js
@@ -0,0 +1,10 @@
+$(bt1).on('click', function () {
+    console.log('1')
+    $('.images').css({ transform: 'translateX(0px)' })
+})
+$('#bt2').on('click', function () {
+    $('.images').css({transform: 'translateX(-850px)'})
+})
+$('#bt3').on('click', function () {
+    $('.images').css({ transform: 'translateX(-1700px)' })
+})
