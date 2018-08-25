70877a35b9645a7501b72d395b6b11a82c226573
MingJiang3 <457918455@qq.com>
2018-08-25 11:26:52 +0800

    2

--- slideShow(3)/main.js
+++ slideShow(3)/main.js
@@ -3,8 +3,8 @@ $('#bt1').on('click', function () {
     $('.images').css({ transform: 'translateX(0px)' })
 })
 $('#bt2').on('click', function () {
-    $('.images').css({transform: 'translateX(-850px)'})
+    $('.images').css({transform: 'translateX(-920px)'})
 })
 $('#bt3').on('click', function () {
-    $('.images').css({ transform: 'translateX(-1700px)' })
+    $('.images').css({ transform: 'translateX(-1840px)' })
 })
