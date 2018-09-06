b4593c6f403c9e4438c3918788c5a6bcf60ad791
MingJiang3 <457918455@qq.com>
2018-09-06 22:55:50 +0800

    0

---
+++ animation-resume/main.js
@@ -0,0 +1,50 @@
+var result = `/*
+ * Hello  我是李名江
+ * 那么多人用文字介绍太单调了
+ * 我就用代码来介绍吧
+ * 首先准备一些样式
+ */
+*{
+    transition:all 1s;
+}
+html{
+    background:rgb(222,222,222);
+    font-size:16px;
+}
+#code{
+    border: 1px solid red;
+    padding: 10px;
+}
+/*我需要代码高亮*/
+.token.selector{
+    color: #690;
+}
+.token.property{
+    color: #905;
+}
+.token.function{
+    color: #dd4a68;
+}
+/*加个3D效果看看*/
+`
+var n = 0
+var timer = setInterval(()=>{
+    n++
+    code.innerHTML = result.substring(0,n)
+    code.innerHTML = Prism.highlight(code.innerHTML,Prism.language.css)
+    styleTag.innerHTML = result.substring(0,n)
+    if (n >= result.length) {
+        window.clearInterval(timer)
+        fn2()
+        fn3()
+    }
+},50)
+
+function fn2(){
+    var paper = document.createElement('div')
+    paper.id = 'paper'
+    document.body.appendChild(paper)
+}
+function fn3(){
+    
+}
